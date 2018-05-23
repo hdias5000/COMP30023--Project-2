3859629050be7aa8d03bc88c8e21f95811556616
Hasitha Dias <30955334+hdias5000@users.noreply.github.com>
2018-05-24 01:59:00 +1000

    hopefully final

--- certcheck.c
+++ certcheck.c
@@ -3,22 +3,23 @@ Program Written By Hasitha Dias
 Student ID: 789929
 University/GitLab Username: diasi
 Email: i.dias1@student.unimelb.edu.au
-Last Modified Date: 20/05/2018
+Last Modified Date: 24/05/2018
 ***/
 
 #include "certcheck.h"
 
 void main(int argc, char const *argv[]) {
     NODE *head,*list;
+    //reads the file
     head = read_CSVfile(head, argv[1]);
 
     list = head;
+    //checks each certificate
     while (list){
         list->valid = check(list->certificate, list->url);
-        printf("%s,%s,%d\n",list->certificate,list->url,list->valid );
         list = list->next;
     }
-
+    //writes to new CSV file
     write_to_file(head);
     free_data(head);
 }
@@ -29,20 +30,25 @@ NODE* read_CSVfile(NODE *head, const char* filename){
     NODE *current, *new = 0;
     int count_nodes = 0;
 
+    //checks if file read correctly
     if(fstream == NULL){
         fprintf(stderr, "Error in reading CSV filename");
         exit(EXIT_FAILURE);
     }
 
+    //saves each line from CSV file
     while((line=fgets(buffer,sizeof(buffer),fstream))!=NULL){
         new  = (NODE *)malloc(sizeof(NODE));
+        assert(new);
 
         record = strtok(line,COMMA);
         new->certificate = malloc(sizeof(record));
+        assert(new->certificate);
         strcpy(new->certificate, record) ;
 
         record = strtok(NULL,EOL);
         new->url = malloc(sizeof(record));
+        assert(new->url);
         strcpy(new->url, record);
 
         new->next = 0;
@@ -61,8 +67,10 @@ int check(char* certificate, char* url){
     X509 *cert = NULL;
     int passed = 1;
 
+    //reads and saves the certificate
     cert = read_certificate(cert,certificate);
 
+    //validates each(required) aspect of the certificate
     if (!validate_date(cert)){
         passed = 0;
     }else if (!validate_CN(url,cert) && !validate_SAN(url,cert)){
@@ -80,10 +88,9 @@ int check(char* certificate, char* url){
 }
 
 X509* read_certificate(X509 *cert, char* certificate){
-    BIO *certificate_bio = NULL;
+    BIO *cert_bio = NULL;
     X509_NAME *cert_issuer = NULL;
     X509_CINF *cert_inf = NULL;
-    STACK_OF(X509_EXTENSION) * ext_list;
 
     //initialise openSSL
     OpenSSL_add_all_algorithms();
@@ -91,28 +98,31 @@ X509* read_certificate(X509 *cert, char* certificate){
     ERR_load_crypto_strings();
 
     //create BIO object to read certificate
-    certificate_bio = BIO_new(BIO_s_file());
+    cert_bio = BIO_new(BIO_s_file());
 
-    //Read certificate into BIO
-    if (!(BIO_read_filename(certificate_bio, certificate))){
+    //reads certificate into BIO
+    if (!(BIO_read_filename(cert_bio, certificate))){
         fprintf(stderr, "Error in reading cert BIO filename");
         exit(EXIT_FAILURE);
     }
 
-    if (!(cert = PEM_read_bio_X509(certificate_bio, NULL, 0, NULL))){
+    //loads certificate from bio
+    if (!(cert = PEM_read_bio_X509(cert_bio, NULL, 0, NULL))){
         fprintf(stderr, "Error in loading certificate");
         exit(EXIT_FAILURE);
     }
     return cert;
-    BIO_free_all(certificate_bio);
+    BIO_free_all(cert_bio);
 }
 
 int validate_date(X509 *cert){
+    //saves not before and not after in ASN1_TIME format
     ASN1_TIME *not_before = X509_get_notBefore(cert);
     ASN1_TIME *not_after = X509_get_notAfter(cert);
 
     int sec,day,sec1,day1;
 
+    //both time differences are checked to see if there are any discrepencies
     ASN1_TIME_diff(&day, &sec, not_before,NULL);
     if ((day<0) || (sec<0)){
         return 0;
@@ -125,34 +135,35 @@ int validate_date(X509 *cert){
 }
 
 int validate_CN(const char *hostname, const X509 *server_cert) {
-	int common_name_loc = -1;
-	X509_NAME_ENTRY *common_name_entry = NULL;
-	ASN1_STRING *common_name_asn1 = NULL;
-	char *common_name_str = NULL;
+	int cn_location = -1;
+	X509_NAME_ENTRY *cn_entry = NULL;
+	ASN1_STRING *cn_asn1 = NULL;
+	char *cn_string = NULL;
 
-	// Find the position of the CN field in the Subject field of the certificate
-	common_name_loc = X509_NAME_get_index_by_NID(X509_get_subject_name((X509 *) server_cert), NID_commonName, -1);
-	if (common_name_loc < 0) {
-        fprintf(stderr, "Error in finding position of CN");
+	//finds the position of the Common Name field in the Subject field of the
+    //certificate
+	cn_location = X509_NAME_get_index_by_NID(X509_get_subject_name(server_cert,
+        NID_commonName, -1);
+	if (cn_location < 0) {
+        fprintf(stderr, "Error in finding position of Common Name");
 		return 0;
 	}
 
-	// Extract the CN field
-	common_name_entry = X509_NAME_get_entry(X509_get_subject_name((X509 *) server_cert), common_name_loc);
-	if (common_name_entry == NULL) {
-        fprintf(stderr, "Error in extracting CN");
+	//extract the Common Name field
+	if (!(cn_entry = X509_NAME_get_entry(X509_get_subject_name(server_cert,
+        cn_location)))) {
+        fprintf(stderr, "Error in extracting Common Name");
 		return 0;
 	}
 
-	// Convert the CN field to a C string
-	common_name_asn1 = X509_NAME_ENTRY_get_data(common_name_entry);
-	if (common_name_asn1 == NULL) {
-        fprintf(stderr, "Error in CN field to string");
+	//convert the Common Name field to a string
+	if (!(cn_asn1 = X509_NAME_ENTRY_get_data(cn_entry))) {
+        fprintf(stderr, "Error in converting Common Name field to string");
 		return 0;
 	}
-	common_name_str = (char *) ASN1_STRING_data(common_name_asn1);
+	cn_string = (char *) ASN1_STRING_data(cn_asn1);
 
-    if (check_wildcards(hostname,common_name_str)) {
+    if (check_wildcards(hostname,cn_string)) {
         return 1;
     }
     return 0;
@@ -160,41 +171,47 @@ int validate_CN(const char *hostname, const X509 *server_cert) {
 
 int validate_SAN(const char *hostname, const X509 *server_cert) {
 	int i;
-	int san_names_nb = -1;
-	STACK_OF(GENERAL_NAME) *san_names = NULL;
+	int san_count = -1;
+	STACK_OF(GENERAL_NAME) *san_list = NULL;
 
-	// Try to extract the names within the SAN extension from the certificate
-	san_names = X509_get_ext_d2i((X509 *) server_cert, NID_subject_alt_name, NULL, NULL);
-	if (san_names == NULL) {
+	//extracts the names within the SAN extension from the certificate
+	if (!(san_list = X509_get_ext_d2i(server_cert, NID_subject_alt_name, NULL,
+        NULL))) {
 		fprintf(stderr, "Error in extracting SAN");
+        return 0;
 	}
-	san_names_nb = sk_GENERAL_NAME_num(san_names);
+	san_count = sk_GENERAL_NAME_num(san_list);
 
-	// Check each name within the extension
-	for (i=0; i<san_names_nb; i++) {
-		const GENERAL_NAME *current_name = sk_GENERAL_NAME_value(san_names, i);
-        char *dns_name = (char *) ASN1_STRING_data(current_name->d.dNSName);
+	//check each name within the extension
+	for (i=0; i<san_count; i++) {
+		const GENERAL_NAME *current_name = sk_GENERAL_NAME_value(san_list, i);
+        char *dns_name = ASN1_STRING_data(current_name->d.dNSName);
+        //checks wildcard
         if (check_wildcards(hostname,dns_name)) {
-            sk_GENERAL_NAME_pop_free(san_names, GENERAL_NAME_free);
+            sk_GENERAL_NAME_pop_free(san_list, GENERAL_NAME_free);
             return 1;
         }
 	}
-	sk_GENERAL_NAME_pop_free(san_names, GENERAL_NAME_free);
+
+	sk_GENERAL_NAME_pop_free(san_list, GENERAL_NAME_free);
 	return 0;
 }
 
 int check_wildcards(const char* hostname, char* dns){
     int diff = 0;
     char firstletter[TWO_CHARACTERS];
+    //extract the first character from Common Name of certificate
     firstletter[0] = dns[0];
     firstletter[1]=TERMINATION_CHARACTER;
 
+    //checks if the first character is a star and if so handles for wildcard
     if (strcmp(firstletter,CHARACTER_STAR)==0) {
         dns = dns+1;
         if (strlen(hostname)>strlen(dns)) {
             diff = strlen(hostname)-strlen(dns);
         }
     }
+
     if (strcasecmp(hostname+diff, dns) == 0) {
         return 1;
     }
@@ -202,10 +219,26 @@ int check_wildcards(const char* hostname, char* dns){
 }
 
 int validate_RSA_key_length(X509 *cert){
-    EVP_PKEY *public_key = X509_get_pubkey(cert);
-    RSA *rsa_key = EVP_PKEY_get1_RSA(public_key);
+    //retrieves RSA key from certificate
+    EVP_PKEY *public_key = NULL;
+    RSA *rsa_key = NULL;
+
+    //extracts(decodes) the public key from the certificate
+    if (!(public_key = X509_get_pubkey(cert))) {
+		fprintf(stderr, "Error in extracting Public Key");
+        return 0;
+	}
+
+    //extracts the referenced key in public_key
+    if (!(rsa_key = EVP_PKEY_get1_RSA(public_key))) {
+		fprintf(stderr, "Error in extracting RSA key");
+        return 0;
+	}
+
+    //converts key_length in RSA modulus bytes to bits
     int key_length = RSA_size(rsa_key) * BITS_IN_BYTE;
     RSA_free(rsa_key);
+
     if (key_length<MAX_BITS){
         return 0;
     }else {
@@ -214,28 +247,48 @@ int validate_RSA_key_length(X509 *cert){
 }
 
 int validate_basic_constraints(X509 *cert){
-    BASIC_CONSTRAINTS *bs;
-    bs = X509_get_ext_d2i(cert, NID_basic_constraints, NULL, NULL);
-    if (bs->ca){
-        BASIC_CONSTRAINTS_free(bs);
+    BASIC_CONSTRAINTS *bc = NULL;
+
+    //extracts the Basic Constrainsts extension
+    if (!(bc = X509_get_ext_d2i(cert, NID_basic_constraints, NULL, NULL))) {
+		fprintf(stderr, "Error in extracting Basic Constrainsts");
+        return 0;
+	}
+
+    if (bc->ca){
+        BASIC_CONSTRAINTS_free(bc);
         return 0;
     }
-    BASIC_CONSTRAINTS_free(bs);
+    BASIC_CONSTRAINTS_free(bc);
     return 1;
 }
 
 int validate_extended_key_usage(X509 *cert){
-    int usageId = 0;
-    const char *kuValue = NULL;
-    STACK_OF(ASN1_OBJECT) *extKu = (STACK_OF(ASN1_OBJECT) *)X509_get_ext_d2i(cert, NID_ext_key_usage,NULL, NULL);
-    if (extKu == NULL) {
+    int valueID = 0;
+    const char *extku_value = NULL;
+    STACK_OF(ASN1_OBJECT) *ext_key_usage = NULL;
+
+    //extracts the Extended Key Usage extension
+    if (!(ext_key_usage = X509_get_ext_d2i(cert, NID_ext_key_usage,NULL, NULL))) {
 		fprintf(stderr, "Error in extracting Extended Key Usage");
+        return 0;
 	}
 
-    while (sk_ASN1_OBJECT_num(extKu) > 0){
-        usageId = OBJ_obj2nid(sk_ASN1_OBJECT_pop(extKu));
-        kuValue = OBJ_nid2sn(usageId);
-        if (strcmp(kuValue,"serverAuth") == 0) {
+    //checks all values of ext key usage to match TLS Server Authentication
+    while (sk_ASN1_OBJECT_num(ext_key_usage) > 0){
+        //extracts the Extended Key Usage ValueID
+        if (!(valueID = OBJ_obj2nid(sk_ASN1_OBJECT_pop(ext_key_usage)))) {
+    		fprintf(stderr, "Error in extracting Extended Key Usage ValueID");
+            return 0;
+    	}
+
+        //extracts the Extended Key Usage extension
+        if (!(extku_value = OBJ_nid2sn(valueID))) {
+    		fprintf(stderr, "Error in extracting Extended Key Usage Value");
+            return 0;
+    	}
+
+        if (strcmp(extku_value,"serverAuth") == 0) {
             return 1;
         }
     }
@@ -249,7 +302,7 @@ void write_to_file(NODE *data) {
         exit(EXIT_FAILURE);
     }
     while (data) {
-        // you might want to check for out-of-disk-space here, too
+        //formatting the data accordingly
         fprintf(f, "%s,%s,%d\n", data->certificate, data->url, data->valid);
         data = data->next;
     }
@@ -259,6 +312,7 @@ void write_to_file(NODE *data) {
 void free_data(NODE *head) {
     NODE *next, *current;
     current = head;
+    //frees data of each node to prevent memory leaks
     while (current) {
         next = current->next;
         free(current->certificate);
--- certcheck.h
+++ certcheck.h
@@ -3,8 +3,10 @@ Program Written By Hasitha Dias
 Student ID: 789929
 University/GitLab Username: diasi
 Email: i.dias1@student.unimelb.edu.au
-Last Modified Date: 20/05/2018
+Last Modified Date: 24/05/2018
 ***/
+
+/////This is the Header for certcheck.c
 
 #ifndef CERTCHECK_H
 #define CERTCHECK_H
@@ -12,14 +14,16 @@ Last Modified Date: 20/05/2018
 #include <stdio.h>
 #include <string.h>
 #include "malloc.h"
-#include <time.h>
+#include "assert.h"
+
 #include <openssl/x509.h>
 #include <openssl/x509v3.h>
 #include <openssl/bio.h>
 #include <openssl/pem.h>
 #include <openssl/err.h>
 
-
+/*This structure stores all the information from the CSV file as a linked
+list(a node for each certificate,domain).*/
 struct node {
     char* certificate;
     char* url;
@@ -28,27 +32,56 @@ struct node {
 };
 typedef struct node NODE;
 
-#define FILENAME_TO_WRITE "babe.csv"
+#define FILENAME_TO_WRITE "output.csv"      //final output filename
 #define TERMINATION_CHARACTER '\0'
-#define TWO_CHARACTERS 2
-#define CHARACTER_STAR "*"
-#define BUFFER_SIZE 1024
+#define TWO_CHARACTERS 2                    //used for wildcards
+#define CHARACTER_STAR "*"                  //used to check for wildcards
+#define BUFFER_SIZE 1024                    //size to store from CSV file
 #define COMMA ","
 #define EOL "\n"
-#define BITS_IN_BYTE 8
-#define MAX_BITS 2048
+#define BITS_IN_BYTE 8                      //converts bytes->bits (RSA)
+#define MAX_BITS 2048                       //mac no. of bits for RSA key
 
+/*This function is used to read the CSV file in to a linked list.*/
 NODE* read_CSVfile(NODE *head, const char* filename);
+
+/*This function calls all the functions required to read the certificate and
+to validate it.*/
 int check(char* certificate, char* url);
+
+/*This function reads each certificate filename as a X509 certificate.*/
 X509* read_certificate(X509 *cert, char* certificate);
+
+/*This function checks if the current date is within the NotBefore and NotAfter
+date of the certificate.*/
 int validate_date(X509 *cert);
+
+/*Checks if given Domain Name from CSV file is same as the Common Name given in
+the certificate(also checks for wildcards).*/
 int validate_CN(const char *hostname, const X509 *server_cert);
+
+/*Checks if given Domain Name from CSV file is same as any of the Alternative
+Subject Names(Common Name) given in the certificate(checks for wildcards).*/
 int validate_SAN(const char *hostname, const X509 *server_cert);
+
+/*When given the hostname and wildcard, checks if the hostname is similar.*/
 int check_wildcards(const char* hostname, char* dns);
+
+/*This function checks if the RSA key length is greater than or atleast equal
+to 2048 bits.*/
 int validate_RSA_key_length(X509 *cert);
+
+/*This function checks if the CA flag of the basic constrainsts is false.*/
 int validate_basic_constraints(X509 *cert);
+
+/*This function checks if Extended Key Usage includes TLS Server
+Authentication.*/
 int validate_extended_key_usage(X509 *cert);
+
+/*This function writes data from a linked list into a CSV file.*/
 void write_to_file(NODE *data);
+
+/*Frees all the data from the linked list.*/
 void free_data(NODE *head);
 
 
